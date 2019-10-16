0f4b1d3fdeb2055f58cbdf48daec173e91676fcb
lucahan <lucahan@foxmail.com>
2019-10-16 17:14:34 +0800

    update

---
+++ src/components/Form/Form.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    export default {
+        name: "Form"
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/Map/Map.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    export default {
+        name: "Map"
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
